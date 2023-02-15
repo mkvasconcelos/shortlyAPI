adad1bad4cc26531581ae37f1d1c03038becde70
Mateus <mateuskvasconcelos@gmail.com>
2023-02-15 20:26:03 -0300

    Second commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+// import router from "./routers/index.js";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+// app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
