d85599e258607b93a1dc801d5d20d1c870717da9
Mateus <mateuskvasconcelos@gmail.com>
2023-02-28 11:06:39 -0300

    Fix: Allow name with accents

--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -3,7 +3,7 @@ import joi from "joi";
 export const signUpSchema = joi.object({
   name: joi
     .string()
-    .pattern(/^[a-zA-Z\s]+$/)
+    .pattern(/^[a-zA-ZÀ-ÖØ-öø-ÿ\s]+$/)
     .required(),
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
