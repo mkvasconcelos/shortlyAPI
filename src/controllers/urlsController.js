27c4bdb3e0118770d4c37f4b016c9224d3900367
Mateus <mateuskvasconcelos@gmail.com>
2023-03-03 12:39:23 -0300

    Update

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -1,4 +1,3 @@
-import connection from "../database/database.js";
 import { nanoid } from "nanoid";
 import UrlsRepository from "../repositories/urlsRepository.js";
 const urlsRepository = new UrlsRepository();
@@ -7,18 +6,8 @@ export async function urlsCreate(_, res) {
   const { userId, url } = res.locals;
   const shortUrl = nanoid();
   try {
-    // await connection.query(
-    //   `INSERT INTO urls (url, "shortUrl", "userId")
-    //      VALUES ($1, $2, $3);`,
-    //   [url, shortUrl, userId]
-    // );
     await urlsRepository.insertUrl(url, shortUrl, userId);
-    // const result = await connection.query(
-    //   `SELECT id, "shortUrl" FROM urls WHERE url = $1 AND "shortUrl" = $2 AND "userId" = $3;`,
-    //   [url, shortUrl, userId]
-    // );
     const response = await urlsRepository.getUrl(url, shortUrl, userId);
-    // return res.status(201).send(result.rows[0]);
     return res.status(201).send(response);
   } catch (err) {
     return res.status(500).send(err);
@@ -29,7 +18,6 @@ export async function urlIdRead(_, res) {
   const { array } = res.locals;
   try {
     delete array.userId;
-    // delete array.createdAt;
     return res.status(200).send(array);
   } catch (err) {
     return res.status(500).send(err);
@@ -42,8 +30,6 @@ export async function urlIdDelete(_, res) {
     if (userId !== userOwner) {
       return res.sendStatus(401);
     }
-    // let query = `DELETE FROM urls WHERE id = ${urlId};`;
-    // await connection.query(query);
     await urlsRepository.deleteUrl(urlId);
     return res.sendStatus(204);
   } catch (err) {
@@ -54,8 +40,6 @@ export async function urlIdDelete(_, res) {
 export async function urlOpen(_, res) {
   const { urlId, url, accesses } = res.locals;
   try {
-    // let query = `UPDATE urls SET accesses = $1 WHERE id = ${urlId};`;
-    // await connection.query(query, [accesses + 1]);
     await urlsRepository.updateAccessesUrl(urlId, accesses);
     return res.status(200).redirect(url);
   } catch (err) {
--- src/middleware/urlsMiddleware.js
+++ src/middleware/urlsMiddleware.js
@@ -1,20 +1,14 @@
-import connection from "../database/database.js";
 import UrlsRepository from "../repositories/urlsRepository.js";
 const urlsRepository = new UrlsRepository();
 
 export async function urlIdValidation(req, res, next) {
   const { id } = req.params;
   try {
-    // let query = `SELECT * FROM urls WHERE id = ${id};`;
-    // const result = await connection.query(query);
-    // if (result.rows.length === 0) {
     const array = await urlsRepository.getUrlById(id);
     if (!array) {
       return res.sendStatus(404);
     }
     res.locals.urlId = id;
-    // res.locals.userOwner = result.rows[0].userId;
-    // res.locals.array = result.rows[0];
     res.locals.userOwner = array.userId;
     res.locals.array = array;
     next();
@@ -26,16 +20,10 @@ export async function urlIdValidation(req, res, next) {
 export async function shortUrlValidation(req, res, next) {
   const { shortUrl } = req.params;
   try {
-    // let query = `SELECT * FROM urls WHERE "shortUrl" = '${shortUrl}';`;
-    // const result = await connection.query(query);
-    // if (result.rows.length === 0) {
     const array = await urlsRepository.getUrlByShortUrl(shortUrl);
     if (!array) {
       return res.sendStatus(404);
     }
-    // res.locals.urlId = result.rows[0].id;
-    // res.locals.url = result.rows[0].url;
-    // res.locals.accesses = result.rows[0].accesses;
     res.locals.urlId = array.id;
     res.locals.url = array.url;
     res.locals.accesses = array.accesses;
