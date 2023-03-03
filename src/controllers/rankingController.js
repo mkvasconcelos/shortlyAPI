e428b466f2567ebd72859c7d3fad3a639ee699a4
Mateus <mateuskvasconcelos@gmail.com>
2023-03-03 11:54:07 -0300

    Test Repository Pattern

--- src/controllers/rankingController.js
+++ src/controllers/rankingController.js
@@ -1,15 +1,26 @@
-import connection from "../database/database.js";
+// import connection from "../database/database.js";
+import RankingRepository from "../repositories/rankingRepository.js";
+const rankingRepository = new RankingRepository();
+
+// export async function rankingRead(_, res) {
+//   try {
+//     let query = ` SELECT users.id, users.name, COUNT(urls.id) AS "linksCount",SUM(urls.accesses) AS "visitCount"
+//                   FROM users
+//                   LEFT JOIN urls ON urls."userId" = users."id"
+//                   GROUP BY users.id
+//                   ORDER BY "visitCount" DESC
+//                   LIMIT 10;`;
+//     const result = await connection.query(query);
+//     return res.status(200).send(result.rows);
+//   } catch (err) {
+//     return res.status(500).send(err);
+//   }
+// }
 
 export async function rankingRead(_, res) {
   try {
-    let query = ` SELECT users.id, users.name, COUNT(urls.id) AS "linksCount",SUM(urls.accesses) AS "visitCount"
-                  FROM users 
-                  LEFT JOIN urls ON urls."userId" = users."id" 
-                  GROUP BY users.id
-                  ORDER BY "visitCount" DESC
-                  LIMIT 10;`;
-    const result = await connection.query(query);
-    return res.status(200).send(result.rows);
+    const topRankedUsers = await rankingRepository.getTopRankedUsers();
+    return res.status(200).send(topRankedUsers);
   } catch (err) {
     return res.status(500).send(err);
   }
